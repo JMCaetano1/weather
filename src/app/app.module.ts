68102388e1e29ee916545047921b64f0f7156376
Marcilio <marcilio@Marcilios-iMac.local>
2020-04-14 11:05:11 -0300

    criação do novo componente, adicionado background

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TemperatureComponent } from './temperature/temperature.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TemperatureComponent
   ],
   imports: [
     BrowserModule,
