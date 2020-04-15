e7a05d085c6e7e5f56aa1802da78703f344d76c9
rmdevjr <rm.devjr@gmail.com>
2020-04-15 13:59:25 -0300

    resolvendo conflitos de pull

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TemperatureComponent } from './temperature/temperature.component';
 import { SearchComponent } from './search/search.component';
-<<<<<<< HEAD
-// import { HttpClient } from "@angular/common/http";
-// import { ApiWeatherService } from './api-weather.service';
-=======
 import { HttpClientModule } from "@angular/common/http";
 import { ApiWeatherService } from './api-weather.service';
->>>>>>> 4e5ac78c9f5ef47aeaf4b07b9e0921cabe7985cd
 
 @NgModule({
   declarations: [
@@ -22,17 +17,10 @@ import { ApiWeatherService } from './api-weather.service';
   imports: [
     BrowserModule,
     AppRoutingModule,
-<<<<<<< HEAD
-    // HttpClient,
-    // ApiWeatherService
-  ],
-  providers: [],
-=======
     HttpClientModule
     
   ],
   providers: [ApiWeatherService],
->>>>>>> 4e5ac78c9f5ef47aeaf4b07b9e0921cabe7985cd
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,9 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-<<<<<<< HEAD
-// import { ApiWeatherService } from "../api-weather.service";
-=======
 import { ApiWeatherService } from "../api-weather.service";
->>>>>>> 4e5ac78c9f5ef47aeaf4b07b9e0921cabe7985cd
 
 @Component({
   selector: 'app-search',
@@ -11,13 +7,6 @@ import { ApiWeatherService } from "../api-weather.service";
   styleUrls: ['./search.component.css']
 })
 export class SearchComponent implements OnInit {
-<<<<<<< HEAD
-
-  constructor() { }
-
-  ngOnInit(): void {
-
-=======
   weather:any 
 
   constructor(private apiservice:ApiWeatherService) { }
@@ -27,7 +16,6 @@ export class SearchComponent implements OnInit {
       this.weather = new Object(res);
       console.log(this.weather.data);
     });
->>>>>>> 4e5ac78c9f5ef47aeaf4b07b9e0921cabe7985cd
 
   }
 
