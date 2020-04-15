483352f92c1940b853661f3240368db7b87304c1
priscilafernandes <pris1@outlook.com.br>
2020-04-15 11:21:54 -0300

    adicionando service api weather

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TemperatureComponent } from './temperature/temperature.component';
 import { SearchComponent } from './search/search.component';
+// import { HttpClient } from "@angular/common/http";
+// import { ApiWeatherService } from './api-weather.service';
 
 @NgModule({
   declarations: [
@@ -14,7 +16,9 @@ import { SearchComponent } from './search/search.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    // HttpClient,
+    // ApiWeatherService
   ],
   providers: [],
   bootstrap: [AppComponent]
