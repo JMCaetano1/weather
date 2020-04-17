d035d75badeb5cb98308dcaeda422682e4d38b2c
Marcilio <marcilio@Marcilios-iMac.local>
2020-04-16 23:06:40 -0300

    Implementando a API na tela e Inicializando os dados de uma cidade default

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -9,25 +9,46 @@ import { ApiWeatherService } from "../api-weather.service";
 export class SearchComponent implements OnInit {
   weather: any
   city: string
+  temp: string
+  windSpeed: string
+  humidity: string
+
 
 
   constructor(private apiservice: ApiWeatherService) { }
 
   ngOnInit(): void {
-
+  this.city = "Garanhuns" 
+  this.sendCity("click")
 
   }
 
   getCity(event) {
     this.city = event.target.value
-    console.log(this.city)
+    console.log(this.city);
   }
 
   sendCity(event) {
     this.apiservice.getWeather(this.city).subscribe((res) => {
       this.weather = new Object(res);
-      console.log(this.weather.data);
+      this.temp = this.weather.data[0].temp
+      this.city = this.weather.data[0].city_name
+      this.humidity = this.weather.data[0].rh
+      this.windSpeed = this.weather.data[0].wind_spd
+      
     });
   }
-
+  
+  newTemp: {}
+  
+  formaterTemp(){
+    this.newTemp = this.temp.split(".",2)
+    this.temp = this.newTemp[0]
+    console.log(this.temp)
+  }
+  
 }
+
+
+
+
